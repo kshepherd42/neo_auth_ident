223f44d73c12af9b4bae6aece66cc844351e15bb
Wettstone <wettstbe@l25011.cs.jmu.edu>
2022-02-23 13:43:39 -0500

    this extracts all the tfidf features

--- extractinatortfidfs.py
+++ extractinatortfidfs.py
@@ -1,16 +1,12 @@
 import pandas as pd
 import numpy as np
 import scipy as sp
-import sys
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 vectorizer = TfidfVectorizer()
-data = pd.read_hdf(sys.argv[1])
+data = pd.read_hdf('sandbox.h5')
 tfidfs = vectorizer.fit_transform(data['file_content'])
 fn = vectorizer.get_feature_names()
-print(data)
-print(fn)
-print(tfidfs)
 np.save("feature_names.npy",fn)
 sp.sparse.save_npz("tfidfs.npz", tfidfs)
 
