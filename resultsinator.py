3709dc17a2d48a5c01cd2bf37aaf8b6d627f675e
kshepherd42 <kaylshep@gmail.com>
2022-02-28 21:40:01 -0500

    Add to results review

--- resultsinator.py
+++ resultsinator.py
@@ -6,29 +6,29 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score, KFold
 from sklearn.neighbors import KNeighborsClassifier
 
-min_files = 10
+num = 7
 
 hdf = pd.read_hdf("sandbox.h5")
 files_by_auth_name = hdf.groupby(['username']).indices
-authors_with_k = dict(filter(lambda x: len(x[1]) >= min_files, files_by_auth_name.items()))
+authors_with_k = dict(filter(lambda x: len(x[1]) >= num, files_by_auth_name.items()))
 authors_with_k = {author: authors_with_k[author] for author in list(authors_with_k)}
 rng = np.random.default_rng(1)
 for k in authors_with_k:
-  authors_with_k[k] = rng.choice(authors_with_k[k], min_files, replace=False, shuffle=False)
+  authors_with_k[k] = rng.choice(authors_with_k[k], num, replace=False, shuffle=False)
 # List of all files sorted by author where
 #each author has exactly k files
 files = np.concatenate(list(authors_with_k.values()))
 
 #generate labels
-y = np.floor(np.arange(len(files)) / min_files)
+y = np.floor(np.arange(len(files)) / num)
 
 #sort data
 cross_val_indicies = np.zeros(len(files), dtype=np.int32)
 # files should always be divizable by k
-fold_size = int(len(files) / min_files)
-for i in range(min_files):
-  cross_val_indicies[i * fold_size:(i + 1) * fold_size] = np.arange(i, len(files), min_files)
-fold_size = int(len(files) / min_files)
+fold_size = int(len(files) / num)
+for i in range(num):
+  cross_val_indicies[i * fold_size:(i + 1) * fold_size] = np.arange(i, len(files), num)
+fold_size = int(len(files) / num)
 # Reorganize labels
 y = y[cross_val_indicies]
 
@@ -42,10 +42,9 @@ for i, cross_val_index in enumerate(cross_val_indicies):
   X[i, : cross_val_indicies.shape[0]] = tfidfs[files[cross_val_index],chosen_indicies]
 
 #code to generate the results
-splits = 7
 rfc = RandomForestClassifier()
 knn = KNeighborsClassifier()
-cv = KFold(n_splits=splits, shuffle=False)
+cv = KFold(n_splits=num, shuffle=False)
 print("\nTree:")
 print(cross_val_score(rfc,X, y,verbose=0, cv=cv))
 print("\nKNN:")
