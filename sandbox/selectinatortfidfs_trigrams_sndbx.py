84494f8177bf9489101403b8d237707cc10427ab
Wettstone <wettstbe@l25011.cs.jmu.edu>
2022-03-23 15:43:07 -0400

    30,000 submission sandbox

---
+++ sandbox/selectinatortfidfs_trigrams_sndbx.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import scipy.sparse
+import sys
+from sklearn.feature_selection import SelectKBest
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import mutual_info_regression
+from sklearn.feature_selection import mutual_info_classif
+
+
+npz = scipy.sparse.load_npz('tfidfs.npz')
+data = pd.read_hdf('sandbox.h5')
+skb = SelectKBest(score_func=chi2, k = 1000)
+X = npz
+le = LabelEncoder()
+y = le.fit_transform(data['username'])
+chosen = skb.fit_transform(X, y)
+fn = skb.get_support(True)
+np.save("chosen.npy", fn)
